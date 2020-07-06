2eb3fc82669f90841e8af7c4c9ea4d807e1ec988
WarrenMfg <warrenmfg@icloud.com>
2020-07-06 19:37:56 -0400

    git init

---
+++ db.js
@@ -0,0 +1,13 @@
+const MongoClient = require('mongodb').MongoClient;
+const { URI } = require('./config');
+const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+module.exports.connect = async () => {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+    return client.db('resources');
+  } catch (err) {
+    console.log(err.message, err.stack);
+  }
+};
---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+const { connect } = require('./db');
+const { ObjectId } = require('mongodb');
+const morgan = require('morgan');
+const cors = require('cors');
+const PORT = process.env.PORT || 50000;
+let db;
+
+// middleware
+app.use(morgan('dev'));
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    maxAge: 7200
+  })
+);
+app.use(express.json());
+
+// api
+app.get('/api/resources/:collection', async (req, res) => {
+  try {
+    // find all from collection and send
+    const docs = await db.collection(req.params.collection).find().toArray();
+    res.send(docs);
+  } catch (err) {
+    console.log(err.message, err.stack);
+    res.status(400).json({ message: 'Bad Request' });
+  }
+});
+
+app.post('/api/resource', async (req, res) => {
+  try {
+    let { collection, description, keywords, resource } = req.body;
+    // split on commas and remove white space
+    keywords = keywords.split(',').reduce((arr, keyword) => {
+      if (keyword) arr.push(keyword.trim());
+      return arr;
+    }, []);
+    // insert doc
+    const newDoc = await db.collection(collection).insertOne({ description, keywords, resource });
+    // send it back
+    res.send(newDoc);
+  } catch (err) {
+    console.log(err.message, err.stack);
+    res.status(400).json({ message: 'Bad Request' });
+  }
+});
+
+app.put('/api/resource/:collection/:id', async (req, res) => {
+  try {
+    const { collection, id } = req.params;
+
+    // find and update doc
+    const updatedDoc = await db
+      .collection(collection)
+      .findOneAndUpdate(
+        { _id: ObjectId.createFromHexString(id) },
+        { $set: { ...req.body } },
+        { returnOriginal: false }
+      );
+    // send it
+    res.send(updatedDoc);
+  } catch (err) {
+    console.log(err.message, err.stack);
+    res.send(400).json({ message: 'Bad Request' });
+  }
+});
+
+// connections
+(async () => {
+  try {
+    db = await connect();
+    app.listen(PORT, () => console.log(`API listening ${PORT}`));
+  } catch (err) {
+    console.log(err.message, err.stack);
+  }
+})();
